066eb5f3f00d04a5b282496d517e7193685a019a
DanielHuds <trainee.daniel.hudson@sigmalabs.co.uk>
2024-06-19 10:37:50 +0100

    schema improvements

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,29 +1,27 @@
 DROP TABLE IF EXISTS price_readings, subscriptions, users, products;
 
 CREATE TABLE products(
-    product_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    url TEXT NOT NULL,
+    product_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    url TEXT UNIQUE NOT NULL,
     product_code TEXT NOT NULL,
     product_name TEXT NOT NULL,
-    CONSTRAINT product_url_unique UNIQUE (url)
 );
 
 CREATE TABLE users (
-    user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    email TEXT NOT NULL,
-    CONSTRAINT user_email_unique UNIQUE (email)
+    user_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
 );
 
 CREATE TABLE subscriptions (
-    subscription_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    subscription_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users (user_id),
     product_id INTEGER NOT NULL REFERENCES products (product_id),
     price_threshold MONEY
 );
 
 CREATE TABLE price_readings (
-    reading_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    reading_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     product_id INTEGER NOT NULL REFERENCES products (product_id),
-    timestamp TIMESTAMP(0) NOT NULL,
+    reading_at TIMESTAMP(0) NOT NULL,
     price MONEY NOT NULL
 );
