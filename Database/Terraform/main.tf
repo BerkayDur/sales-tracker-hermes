ef7e60559d7fcfecd7581a0b347830c8a1fb6a4f
Faris Abulula <abululb02@gmail.com>
2024-06-19 11:42:30 +0100

    added security group and variables

--- Database/Terraform/main.tf
+++ Database/Terraform/main.tf
@@ -16,21 +16,53 @@ provider "aws" {
 # terraform import aws_db_instance.database c11-hermes-db
 resource "aws_db_instance" "database" {
     allocated_storage                     = 20
-    db_name                               = "sales"
+    db_name                               = var.DB_NAME
     availability_zone                     = "eu-west-2a"
     copy_tags_to_snapshot                 = true
-    db_subnet_group_name                  = "c11-public-subnet-group"
+    db_subnet_group_name                  = var.DB_SUBNET
     engine                                = "postgres"
     engine_version                        = "16.3"
-    identifier                            = "c11-hermes-db"
+    identifier                            = var.RDS_NAME
     instance_class                        = "db.t3.micro"
     license_model                         = "postgresql-license"
     network_type                          = "IPV4"
-    port                                  = 5432
+    port                                  = var.DB_PORT
     password                              = var.DB_PASSWORD
     publicly_accessible                   = true
     skip_final_snapshot                   = true
     storage_encrypted                     = true
-    username                              = "postgres"
-    vpc_security_group_ids                = ["sg-0599fe57129262ed0"]
+    username                              = var.DB_USERNAME
+    vpc_security_group_ids                = [aws_security_group.c11_hermes_sg.id]
+}
+
+# terraform import aws_security_group.c11_hermes_sg sg-0599fe57129262ed0
+resource "aws_security_group" "c11_hermes_sg" {
+    name        = var.SG_NAME
+    description = "Allows access to port ${var.DB_PORT}"
+    egress      = [
+        {
+            cidr_blocks      = ["0.0.0.0/0"]
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+    ]
+    ingress     = [
+        {
+            cidr_blocks      = ["0.0.0.0/0"]
+            description      = ""
+            from_port        = var.DB_PORT
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = var.DB_PORT
+        },
+    ]
 }
--- Database/Terraform/outputs.tf
+++ Database/Terraform/outputs.tf
@@ -1,22 +1,18 @@
 # This file outputs data from the database
 # The output data is useful for connecting to the database
 
-data "aws_db_instance" "database" {
-  db_instance_identifier = "c11-hermes-db"
-}
-
 output "address" {
-  value = data.aws_db_instance.database.address
+  value = aws_db_instance.database.address
 }
 
 output "port" {
-  value = data.aws_db_instance.database.port
+  value = aws_db_instance.database.port
 }
 
 output "db_name" {
-  value = data.aws_db_instance.database.db_name
+  value = aws_db_instance.database.db_name
 }
 
 output "host" {
-  value = data.aws_db_instance.database.master_username
+  value = aws_db_instance.database.username
 }
