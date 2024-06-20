ca01cdf50a08600b02b4a60a218c8e5c15bf41b9
danfh00 <danielfhudson@yahoo.com>
2024-06-20 12:08:55 +0100

    terraform for the provision lambda added

---
+++ ETL/Provision/aws_infrastructure/variables.tf
@@ -0,0 +1,43 @@
+variable "ACCESS_KEY" {
+    type = string
+  
+}
+variable "SECRET_ACCESS_KEY" {
+    type = string
+  
+}
+variable "AWS_REGION" {
+    type = string
+    default = "eu-west-2"
+  
+}
+
+variable "DB_HOST" {
+    type = string
+}
+
+variable "DB_PORT" {
+    type = number
+    default = 1433
+}
+
+variable "DB_PASSWORD" {
+    type = string
+}
+
+variable "DB_USER" {
+    type = string
+}
+
+variable "DB_NAME" {
+    type = string
+}
+
+variable "DB_SCHEMA" {
+    type = string
+}
+
+variable "PROCESSING_BATCH_SIZE" {
+    type = number
+}
+
