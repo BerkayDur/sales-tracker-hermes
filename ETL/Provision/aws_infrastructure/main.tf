ed20e79aa0f1128a53cb5ac95e77fa6203e6b5c9
danfh00 <danielfhudson@yahoo.com>
2024-06-20 15:43:49 +0100

    PR changes made

--- ETL/Provision/aws_infrastructure/main.tf
+++ ETL/Provision/aws_infrastructure/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = var.AWS_REGION
+    region     = var.AWS_REGION
     access_key = var.ACCESS_KEY
     secret_key = var.SECRET_ACCESS_KEY 
 }
@@ -8,7 +8,7 @@ resource "aws_iam_role" "provision_lambda_role" {
   name = "lambda_execution_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version   = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
@@ -21,15 +21,6 @@ resource "aws_iam_role" "provision_lambda_role" {
   })
 }
 
-resource "aws_ecr_repository" "provision-repo" {
-  name                 = "hermes-provision-lambda"
-  image_tag_mutability = "MUTABLE"
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-}
-
-
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.provision_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
@@ -37,11 +28,16 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
 
 resource "aws_lambda_function" "provision_lambda" {
   function_name = "c11-hermes-provision"
-  role = aws_iam_role.provision_lambda_role.arn
-  package_type = "Image"
-  image_uri = "129033205317.dkr.ecr.eu-west-2.amazonaws.com/c11-hermes-provision:latest"
+  role          = aws_iam_role.provision_lambda_role.arn
+  package_type  = "Image"
+  image_uri     = "129033205317.dkr.ecr.eu-west-2.amazonaws.com/c11-hermes-provision:latest"
   architectures = ["x86_64"]
   timeout       = 30
+
+  logging_config {
+    log_format        = "Text"
+    log_group         = "/aws/lambda/c11-hermes-clean_lambda"
+    }
 
   environment {
     variables = {
--- ETL/Provision/aws_infrastructure/outputs.tf
+++ ETL/Provision/aws_infrastructure/outputs.tf
@@ -1,3 +1,3 @@
-# output "lambda_arn" {
-#   value = aws_lambda_function.provision_lambda.arn
-# }
+output "lambda_arn" {
+  value = aws_lambda_function.provision_lambda.arn
+}
