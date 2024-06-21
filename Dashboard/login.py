8b923bd989007aee0da0b6f32dccaf801591d13d
Faris Abulula <abululb02@gmail.com>
2024-06-21 18:18:52 +0100

    added login authentication

--- Dashboard/login.py
+++ Dashboard/login.py
@@ -1,34 +1,96 @@
 """Login page for streamlit"""
 
+from os import _Environ, environ as ENV
+import logging
+import re
+
 import streamlit as st
+from dotenv import load_dotenv
+from psycopg2 import connect
+from psycopg2.extensions import connection
 
-if 'logged_in' not in st.session_state:
-    st.session_state.logged_in = False
+EMAIL = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
 
 
-def authenticate(email) -> bool:
-    """Function to authenticate email"""
-    return "@" in email and "." in email
+def get_connection(config: _Environ) -> connection:
+    "Establishes a connection with the database"
+    return connect(
+        user=config["DB_USER"],
+        password=config["DB_PASSWORD"],
+        host=config["DB_HOST"],
+        port=config["DB_PORT"],
+        database=config["DB_NAME"]
+    )
+
+
+def authenticate(email) -> tuple | None:
+    """Find email in database"""
+    logging.info("Searching for %s in database", email)
+    load_dotenv()
+    conn = get_connection(ENV)
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT email FROM users
+            WHERE email = %s""", (email,))
+        data = cur.fetchone()
+    return data
+
+
+def add_email(email):
+    """Add email to database"""
+    logging.info("Adding %s to database", email)
+    load_dotenv()
+    conn = get_connection(ENV)
+    with conn.cursor() as cur:
+        cur.execute("""
+            INSERT email FROM users
+            WHERE email = %s""", (email,))
+        data = cur.fetchone()
+    return data
 
 
 def login_page() -> None:
     """Login page"""
     st.title("Login Page")
 
-    email = st.text_input("Please enter your email to access the website:")
+    st.write("trainee.faris.abulula@sigmalabs.co.uk")
+    email = st.text_input(
+        "Please enter your email to access the website:")
 
     if st.button("Login"):
-        st.write(email)
+        logging.info("Login button clicked with %s", email)
         if authenticate(email):
+            logging.info("%s logged in", email)
             st.success(f"Welcome! You are logged in with email: {email}")
             st.session_state.logged_in = True
             st.session_state.email = email
-            st.experimental_rerun()
+            st.rerun()
+        else:
+            st.error(f"Invalid email address {email}. Please try again.")
+
+    st.write("---")
+
+    st.title("Sign up")
+
+    email = st.text_input(
+        "Please enter your email to sign up to the website:")
+
+    if st.button("Sign up"):
+        logging.info("Sign up button clicked")
+        st.write(email)
+        if re.search(EMAIL, email):
+            st.success(f"Welcome! You are logged in with email: {email}")
+            st.session_state.logged_in = True
+            st.session_state.email = email
+            st.rerun()
         else:
             st.error("Invalid email address. Please try again.")
 
 
 def main() -> None:
+    """Checking login information"""
+    if 'logged_in' not in st.session_state:
+        st.session_state.logged_in = False
     if st.session_state.logged_in:
         st.switch_page("pages/1_price.py")
     else:
@@ -36,4 +98,5 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level="INFO")
     main()
--- Dashboard/pages/1_price.py
+++ Dashboard/pages/1_price.py
@@ -7,21 +7,41 @@ def price_tracker_page() -> None:
     """Product Price Tracker Page"""
     st.title("Product Price Tracker")
 
+    with st.expander("List of Compatible websites", expanded=True):
+        st.write("ASOS")
+
     product_url = st.text_input("Enter the product URL:")
 
     if st.button("Track Price"):
         if product_url:
-            price = "£20"
-            if price:
-                st.success(f"The current price of the product is: {price}")
-            else:
-                st.error(
-                    "Unable to fetch the price. Please check the URL or try again later.")
+            subscribe(product_url)
         else:
             st.error("Please enter a valid product URL.")
 
 
+def subscribe(product_url):
+    """Tracks product"""
+    exist = check_url_exists(product_url)
+    if exist:
+        return st.success(
+            "Product url is already being tracked. You have been subscribed.")
+
+    price = get_price(product_url)
+    if price:
+        return st.success(f"The current price of the product is: {price}")
+    return st.error("Unable to fetch the price. Please check the URL.")
+
+
+def check_url_exists(product_url) -> bool:
+    return False
+
+
+def get_price(product_url) -> str:
+    return "£20"
+
+
 def main() -> None:
+    """Checking login information"""
     if 'logged_in' in st.session_state and st.session_state.logged_in:
         price_tracker_page()
     else:
