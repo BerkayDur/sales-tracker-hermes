3167762625c9d1afbde84c4bb16bdae743e73d51
danfh00 <danielfhudson@yahoo.com>
2024-06-24 15:34:06 +0100

    pylint improved

--- ETL/Provision/provision_lambda.py
+++ ETL/Provision/provision_lambda.py
@@ -33,7 +33,8 @@ def read_database(conn: connection):
         raise TypeError(
             'A cursor can only be constructed from a Psycopg2 connection object')
     with get_cursor(conn) as cur:
-        cur.execute("""SELECT DISTINCT ON (product_id) product_id, product_code, url, price, website_name
+        cur.execute("""SELECT DISTINCT ON (product_id) product_id, product_code,
+                    url, price, website_name
                     FROM products
                     LEFT JOIN price_readings USING (product_id)
                     LEFT JOIN websites USING (website_id)
