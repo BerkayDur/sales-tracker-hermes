3f71102b61aed445c50c4adfd304648ed27d2d3d
BerkayDur <Berkaydur138@gmail.com>
2024-06-24 10:37:04 +0100

    update dictionary access and ENV typehint in ses_get_emails.py

--- Dashboard/utilities/ses_get_emails.py
+++ Dashboard/utilities/ses_get_emails.py
@@ -1,6 +1,6 @@
 '''utility functions to get all/verified/unverified emails from ses.'''
 
-from os import environ as CONFIG
+from os import _Environ, environ as CONFIG
 import logging
 
 from dotenv import load_dotenv
@@ -9,7 +9,7 @@ from boto3 import client as boto_client
 from botocore.client import BaseClient
 from mypy_boto3_ses.client import SESClient as ses_client
 
-def get_ses_client(config: dict) -> ses_client:
+def get_ses_client(config: _Environ) -> ses_client:
     '''Returns an ses client from a configuration.'''
     return boto_client(
         'ses',
