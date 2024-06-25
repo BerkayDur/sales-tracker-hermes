0aac15a20e129cd941b5fe8dc3b2c4aaa1d46661
BerkayDur <Berkaydur138@gmail.com>
2024-06-25 13:14:35 +0100

    fix merge

--- Database/query.sql
+++ Database/query.sql
@@ -3,11 +3,13 @@
 -- Example queries for each table
 SELECT * FROM users;
 SELECT * FROM products;
+SELECT * FROM websites;
 SELECT * FROM subscriptions;
 SELECT * FROM price_readings;
 
 -- A query across all of the tables
 SELECT * FROM products
+FULL OUTER JOIN websites USING (website_id)
 FULL OUTER JOIN price_readings USING (product_id)
 FULL OUTER JOIN subscriptions USING (product_id)
 FULL OUTER JOIN users USING (user_id);
