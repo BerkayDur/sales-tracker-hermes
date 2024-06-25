fc41c62fe263859820655a75b7d6c4874ec8bfd0
BerkayDur <Berkaydur138@gmail.com>
2024-06-25 09:13:03 +0100

    improve linting of price.py

--- Dashboard/pages/price.py
+++ Dashboard/pages/price.py
@@ -1,47 +1,52 @@
 """Price tracker page for streamlit"""
 
-from os import environ as CONFIG
+from os import _Environ, environ as CONFIG
 import logging
 from datetime import datetime
 
 from dotenv import load_dotenv
 import streamlit as st
-from streamlit.delta_generator import DeltaGenerator
 import pandas as pd
 import altair as alt
+from psycopg2.extensions import connection
+
 
 from navigation import make_sidebar
-
 from utilities.helpers import get_cursor, get_connection
 from utilities.load import load_product_data
 
 def can_parse_as_float(to_check: any) -> bool:
+    """Determines if a value can be parsed as a float.
+    Returns True on success, else False."""
     try:
         float(to_check)
         return True
     except ValueError:
         return False
 
-def get_supported_websites(conn) -> list:
+def get_supported_websites(conn: connection) -> list:
     """Gets a list of supported websites."""
     with get_cursor(conn) as cur:
         cur.execute('SELECT website_name from websites;')
         websites = cur.fetchall()
     return [website['website_name'] for website in websites]
 
-def get_user_id(conn, email: str) -> int | None:
+def get_user_id(conn: connection, email: str) -> int | None:
+    """Get a user id from the database based on an email."""
     with get_cursor(conn) as cur:
         cur.execute('SELECT user_id from users WHERE email = %s;', (email,))
         user_id = cur.fetchone()
     return user_id.get('user_id')
 
-def get_product_id(conn, url: str) -> int | None:
+def get_product_id(conn: connection, url: str) -> int | None:
+    """Gets the products id from the database given a url."""
     with get_cursor(conn) as cur:
         cur.execute('SELECT product_id from products WHERE url = %s;', (url,))
         product_id = cur.fetchone()
     return product_id.get('product_id')
 
-def is_subscription_in_table(conn, user_id: int, product_id: int) -> bool:
+def is_subscription_in_table(conn: connection, user_id: int, product_id: int) -> bool:
+    """Determines if a user is subscribed to a particular product."""
     with get_cursor(conn) as cur:
         cur.execute('''SELECT * FROM subscriptions
                         WHERE user_id = %s AND product_id = %s''',
@@ -49,7 +54,11 @@ def is_subscription_in_table(conn, user_id: int, product_id: int) -> bool:
         counts = cur.fetchone()
     return counts is not None
 
-def insert_subscription_into_db(conn, user_id: int, product_id: int, price_threshold: float| int | None) -> bool:
+def insert_subscription_into_db(
+        conn: connection, user_id: int, product_id: int,
+        price_threshold: float| int | None) -> bool:
+    """Inserts a new subscription into the subscriptions table.
+    Returns True on success, else False."""
     try:
         with get_cursor(conn) as cur:
             cur.execute('''INSERT INTO subscriptions
@@ -61,7 +70,10 @@ def insert_subscription_into_db(conn, user_id: int, product_id: int, price_thres
         st.write(e)
         return False
 
-def update_db_to_subscribe(config: dict, product_url: str, price_threshold: bool | None, user_email: str) -> bool:
+def update_db_to_subscribe(
+        config: _Environ, product_url: str, price_threshold: bool | None, user_email: str) -> bool:
+    """Inserts a subscription into a database, if it doesn't already exist.
+    Returns True on success, else False"""
     conn = get_connection(config)
     user_id = get_user_id(conn, user_email)
     if not user_id:
@@ -88,13 +100,15 @@ def subscribe_to_product(config: dict, product_url: str, price_threshold: bool |
     try:
         if price_threshold == '':
             price_threshold = None
-        return  update_db_to_subscribe(config, product_url, price_threshold, st.session_state['email'])
+        return  update_db_to_subscribe(config, product_url,
+                                       price_threshold, st.session_state['email'])
     except (TypeError, ValueError):
         st.warning('Unable to subscribe to product!')
         return False
 
 
-def get_subscribed_products(config: dict, email: str) -> None:
+def get_subscribed_products(config: _Environ, email: str) -> list[dict]:
+    """Get a list of all products subscribed by user, email."""
     conn = get_connection(config)
     with get_cursor(conn) as cur:
         cur.execute('''SELECT product_id, website_name, url, product_name, price_threshold
@@ -106,7 +120,8 @@ def get_subscribed_products(config: dict, email: str) -> None:
         subscribed_products = cur.fetchall()
     return subscribed_products
 
-def get_price_readings(config, product_id: int) -> pd.DataFrame | None:
+def get_price_readings(config: _Environ, product_id: int) -> pd.DataFrame | None:
+    """Get a pandas DataFrame of all price readings for a particular product."""
     conn = get_connection(config)
     with get_cursor(conn) as cur:
         cur.execute('''SELECT * FROM price_readings WHERE product_id = %s;''', (product_id,))
@@ -117,7 +132,9 @@ def get_price_readings(config, product_id: int) -> pd.DataFrame | None:
     price_readings['price'] = price_readings['price'].apply(float)
     return price_readings
 
-def get_encode_price_reading(price_readings: pd.DataFrame, price_threshold: float | None) -> alt.ChartDataType:
+def get_encode_price_reading(
+        price_readings: pd.DataFrame, price_threshold: float | None) -> alt.ChartDataType:
+    """Encode the price readings for a particular product as an altair Chart."""
     price_reading_at = price_readings[['price', 'reading_at']].sort_values(by='reading_at')
     fake_new_data = price_reading_at.iloc[[-1]]
     fake_new_data['reading_at'] = datetime.now()
@@ -140,13 +157,21 @@ def get_encode_price_reading(price_readings: pd.DataFrame, price_threshold: floa
     return chart.mark_point() + chart.mark_line()
 
 
-def display_subscribed_product(config, product_information: dict) -> None:
+def display_subscribed_product(config: _Environ, product_information: dict) -> None:
+    """For a particular product, get the price readings and display a container
+    for that product, containing relevant information."""
     price_readings = get_price_readings(config, product_information['product_id'])
 
     with st.container(border=True):
-        st.write('**%s** - **[%s](%s)**'% (product_information['website_name'], product_information['product_name'], product_information['url']), )
+        st.write('**%s** - **[%s](%s)**' % (product_information['website_name'],
+                                            product_information['product_name'],
+                                            product_information['website_name']))
+#         st.write(f'**{product_information['website_name']}** -\
+# **[{product_information['product_name']}]\
+# ({product_information['url']})**'(product_information['website_name']))
         if price_readings is not None:
-            encoded_price_readings = get_encode_price_reading(price_readings, float(product_information['price_threshold']))
+            encoded_price_readings = get_encode_price_reading(
+                price_readings, float(product_information['price_threshold']))
             st.altair_chart(encoded_price_readings)
 
         else:
@@ -154,7 +179,7 @@ def display_subscribed_product(config, product_information: dict) -> None:
         st.write(price_readings)
 
 
-def price_tracker_page(config: dict) -> None:
+def price_tracker_page(config: _Environ) -> None:
     """Product Price Tracker Page"""
     conn = get_connection(config)
     st.title("Product Price Tracker")
