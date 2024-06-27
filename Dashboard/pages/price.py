2b6a2449926baa98ff6eeac93977a0d8d5cf21ca
BerkayDur <Berkaydur138@gmail.com>
2024-06-27 15:37:27 +0100

    fix reimport

--- Dashboard/pages/price.py
+++ Dashboard/pages/price.py
@@ -3,9 +3,7 @@
 from os import environ as CONFIG
 import logging
 from datetime import datetime
-from datetime import datetime
 
-from dotenv import load_dotenv
 from dotenv import load_dotenv
 import streamlit as st
 import streamlit_nested_layout # pylint: disable=unused-import
