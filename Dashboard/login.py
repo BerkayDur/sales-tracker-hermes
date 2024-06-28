07cd71bde03b41a737845092290a4ed247e149cb
BerkayDur <Berkaydur138@gmail.com>
2024-06-28 11:35:06 +0100

    remove unused sql file

--- Dashboard/login.py
+++ Dashboard/login.py
@@ -9,12 +9,11 @@ import streamlit as st
 from dotenv import load_dotenv
 from psycopg2.extensions import connection
 
-from helpers import (get_connection)
+from helpers import get_connection
 from navigation import make_sidebar
 from custom_styling import apply_custom_styling
 
 EMAIL_PATTERN = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
-
 
 def authenticate(conn: connection, email: str) -> tuple | None:
     """Find email in database"""
